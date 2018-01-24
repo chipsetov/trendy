c635f7985f250d07cff578becf5e301f124692ee
test <test@test.com>
2018-01-24 22:05:58 +0200

    create basic theme files

---
+++ wp-content/themes/trendy/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 1/24/2018
+ * Time: 9:27 PM
+ */
+
---
+++ wp-content/themes/trendy/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo wp_get_document_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
