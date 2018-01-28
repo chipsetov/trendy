353b2ca89f47052cabd8c4ad82ab3fd29fcc48d2
test <test@test.com>
2018-01-29 00:06:15 +0200

    fix some css. make content-video.php

--- wp-content/themes/trendy/content-video.php
+++ wp-content/themes/trendy/content-video.php
@@ -1,12 +1,32 @@
-<?php the_content(); ?>
+<div class="global-content">
+    <div class="post-content-music"><?php the_content(); ?></div>
+    <div class="row">
+        <div class="col-sm-2 left-dates"><img
+                    src="<? echo get_template_directory_uri() . "/images/category-icon-video.png" ?>"
+                    alt="category-icon-video.png">
+            <div class="post-date-number"><?php the_time('d'); ?></div>
+            <div class="post-date-mounth"><?php the_time('M'); ?></div>
+        </div>
 
-<div class="post-title-blog-post"><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></div>
-<?php echo get_the_author(); ?></p>
-<?php the_category(); ?>
-<?php
-$comments_count = wp_count_comments( get_the_ID() );
-echo $comments_count->approved . " comments";
-?>
-<small>Posted on: <?php the_time('d M'); ?></small>
-<?php the_excerpt(); ?>
-<hr>
+        <div class="col-sm-10 standard-post">
+            <?php the_title(sprintf('<h1 class="post-title-video"><a href="%s">', esc_url(get_permalink())), '</a></h1>'); ?>
+
+            <div class="post-metadata-video">
+                <i class="fa fa-user" aria-hidden="true">&nbsp;<?php echo get_the_author(); ?></p></i>
+                <i class="fa fa-folder-open" aria-hidden="true">&nbsp;<?php the_category(', '); ?></i>
+                <i class="fa fa-comment" aria-hidden="true">&nbsp;<?php
+                    $comments_count = wp_count_comments(get_the_ID());
+                    echo $comments_count->approved . " comments";
+                    ?>  </i>
+            </div>
+
+            <div class="post-video-excerpt">
+                <?php the_excerpt(); ?>
+            </div>
+            <div class="post-video-readmore">
+                <a href="<?php echo get_permalink(); ?>">READ MORE</a>
+            </div>
+        </div>
+
+    </div>
+</div>
