3bda294ae807406ab8f9d6ea57e6982a6e30ae21
test <test@test.com>
2018-01-28 14:29:50 +0200

    our-blog-title

--- wp-content/themes/trendy/index.php
+++ wp-content/themes/trendy/index.php
@@ -1,4 +1,12 @@
 <?php get_header(); ?>
+    <div id="our-blog-title" class="row">
+        <div class="container">
+            <div class="col-xs-12 our-blog-title">
+                <h1>OUR BLOG</h1>
+                <p>Duis vitae velit mollis, congue nisi dignissim, pellentesque lorem</p>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="container">
             <div class="col-xs-12 col-sm-8">
