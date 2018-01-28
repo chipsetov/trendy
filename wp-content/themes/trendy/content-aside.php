fa9e8cd7a27bf182ae20efa76f88d4d146197fc7
test <test@test.com>
2018-01-28 22:22:12 +0200

    make acide post

--- wp-content/themes/trendy/content-aside.php
+++ wp-content/themes/trendy/content-aside.php
@@ -1,18 +1,17 @@
 <div class="global-content">
-    <div class="post-content-music"><?php the_content(); ?></div>
+    <div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
     <div class="row">
         <div class="col-sm-2 left-dates"><img
-                    src="<? echo get_template_directory_uri() . "/images/category-icon-music.png" ?>"
-                    alt="category-icon-quote.png">
+                    src="<? echo get_template_directory_uri() . "/images/category-icon-standard.png" ?>"
+                    alt="category-icon-standard.png">
             <div class="post-date-number"><?php the_time('d'); ?></div>
             <div class="post-date-mounth"><?php the_time('M'); ?></div>
         </div>
 
-        <div class="col-sm-10 music-post">
+        <div class="col-sm-10 standard-post">
+            <?php the_title(sprintf('<h1 class="post-title-standard"><a href="%s">', esc_url(get_permalink())), '</a></h1>'); ?>
 
-            <div class="post-title-music"><?php the_title(); ?> </div>
-
-            <div class="post-metadata-music">
+            <div class="post-metadata-standard">
                 <i class="fa fa-user" aria-hidden="true">&nbsp;<?php echo get_the_author(); ?></p></i>
                 <i class="fa fa-folder-open" aria-hidden="true">&nbsp;<?php the_category(', '); ?></i>
                 <i class="fa fa-comment" aria-hidden="true">&nbsp;<?php
@@ -21,10 +20,12 @@
                     ?>  </i>
             </div>
 
-            <div class="post-audio-excerpt">
+            <div class="post-standard-excerpt">
                 <?php the_excerpt(); ?>
             </div>
-
+            <div class="post-standard-readmore">
+                <a href="<?php echo get_permalink(); ?>">READ MORE</a>
+            </div>
         </div>
 
     </div>
