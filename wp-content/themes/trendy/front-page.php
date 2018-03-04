d5ac0ecc3d9c21d0843c64dc20fb0cd555c741ef
test <test@test.com>
2018-03-04 20:06:48 +0200

    make 'we are' section

--- wp-content/themes/trendy/front-page.php
+++ wp-content/themes/trendy/front-page.php
@@ -1,8 +1,24 @@
 <?php get_header(); ?>
 
+<section id="designing">
+    <div class="container">
 
+        <div class="titles">
 
-<div class="video-section">
+            <h1>We are</h1>
+            <hr />
+            <h1>designing</h1>
+            <p>And We Are Doing It Well</p>
+
+        </div>
+        <div class="buttonblock">
+        <a href="#" class="button"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+
+</section>
+
+<section class="video-section">
     <video id="video-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
         <source src="<? echo get_template_directory_uri() . "/video/video.mp4" ?>" type="video/mp4">
         Video not supported
@@ -13,7 +29,7 @@
             WITH VIDEO BACKGROUND</h1>
 
     </div>
-</div>
+</section>
 
 
 <div id="contact-us">
